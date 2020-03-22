3843dbe7b9a62ea7baece87a5ebfcd51bb6a5489
eatmyshorts23 <62490669+eatmyshorts23@users.noreply.github.com>
2020-03-21 22:41:25 -0500

    Update basicVariables.js

--- basicVariables.js
+++ basicVariables.js
@@ -1,10 +1,10 @@
 const basicVariables = () => {
   // Create a variable here and give it a value of "Hello World!"
   // let {name of variable} = "Hello World!";
-  
+  let test = "Hello World!";
   // Return the variable below 
   // return {name of variable};
-  
+  return test;
 }
 
 module.exports = basicVariables;
